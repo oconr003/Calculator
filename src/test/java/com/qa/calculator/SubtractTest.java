190e183abfc8c5d754a4343c770eef738e8499f5
oconr003 <rachel-oc@hotmail.co.uk>
2018-04-19 10:21:23 +0100

    Subtract feature using TDD

---
+++ src/test/java/com/qa/calculator/SubtractTest.java
@@ -0,0 +1,20 @@
+package com.qa.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SubtractTest {
+
+	
+	private Subtractor subtractor;
+	
+	@Test
+	public void test() {
+		subtractor = new Subtractor();
+		int expectedValue = 2;
+		int actualValue = subtractor.subtract(4,2);
+		Assert.assertEquals(expectedValue, actualValue);
+	
+	}
+
+}
